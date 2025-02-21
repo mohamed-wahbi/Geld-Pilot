0a8613559cdc055c90462f645c78b5dec6064f48
mohamed-wahbi <wahbisj@gmail.com>
2025-02-21 10:39:54 +0100

    Install Dependencies: bcrypt, cors, dotenv, express, express-async-handler, joi, jsonwebtoken, mongoose, nodemon.

--- Project_Codes/PFE_Project/Backend_Project_Codes/config/connect.js
+++ Project_Codes/PFE_Project/Backend_Project_Codes/config/connect.js
@@ -5,7 +5,7 @@ const mongidb_URL = process.env.DB_URL
 
 
 mongoose.connect(mongidb_URL)
-.then(()=>console.log('Connecting with DB *_*'))
+.then(()=>console.log('Connecting with DB *_* '))
 .catch((error)=>console.log(error))
 
 module.exports=mongoose;
