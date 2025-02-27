1e1e32dc626868227522a312b056a519456ca3d2
mohamed-wahbi <wahbisj@gmail.com>
2025-02-27 18:35:04 +0100

    Fixing the initial design of the dashboard and configuring the routing system between AI tools.

--- Project_Codes/PFE_Project/SPFX_Project_Codes/src/webparts/iaSupport/components/IaSupport.tsx
+++ Project_Codes/PFE_Project/SPFX_Project_Codes/src/webparts/iaSupport/components/IaSupport.tsx
@@ -4,13 +4,42 @@ const AOS = require("aos");
 import "aos/dist/aos.css";
 
 const IaSupport: React.FC = () => {
+  const [activeTab, setActiveTab] = React.useState('partners');
+
   React.useEffect(() => {
     AOS.init({ duration: 1500, once: true });
   }, []);
 
+
+
   return (
-    <div className={styles.IaSupport}>
-      IaSupport
+    <div className={styles.DashComp}>
+      {/* Header Section */}
+      <div className={styles.headerDash}>
+        <div className={styles.titleDash}>
+          <h5 className={styles.titleTextDash}>AI Support Tools</h5>
+          <img
+            src={require("../assets/logo-removebg-preview.png")}
+            alt="logo"
+            className={styles.logoImgDash}
+          />
+        </div>
+
+        {/* Navigation Tabs */}
+        <div className={styles.navLinks}>
+          <p className={activeTab === "partners" ? styles.link + " " + styles.active : styles.link}
+            onClick={() => setActiveTab("partners")}>
+            Smarty
+          </p>
+          <p className={activeTab === "create" ? styles.link + " " + styles.active : styles.link}
+            onClick={() => setActiveTab("create")}>
+            Geld-Bot
+          </p>
+        </div>
+
+      </div>
+
+     
     </div>
   );
 };
