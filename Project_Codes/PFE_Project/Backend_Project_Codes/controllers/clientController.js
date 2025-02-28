b1ab910881223cf90d7e6aef83e09818273da1df
mohamed-wahbi <wahbisj@gmail.com>
2025-02-28 19:53:50 +0100

    Create GetAll clients request.

--- Project_Codes/PFE_Project/Backend_Project_Codes/controllers/clientController.js
+++ Project_Codes/PFE_Project/Backend_Project_Codes/controllers/clientController.js
@@ -20,7 +20,6 @@ module.exports.createClientCtrl = asyncHandler (async (req,res) => {
     if(findClient){
         return res.status(400).json({message: "One Client with this email is Arady existe!"})
     }
-    
     // create client 
     const newClient = await Client.create(req.body)
     if(!newClient){
@@ -29,7 +28,27 @@ module.exports.createClientCtrl = asyncHandler (async (req,res) => {
 
 
     res.status(201).json({ message: "Client created successfully", client: newClient });
+})
 
 
-    
+
+
+/*--------------------------------------------------
+* @desc    Get all clients
+* @router  /api/client/get_all
+* @methode GET
+* @access  only admin
+----------------------------------------------------*/
+module.exports.getAllClientCtrl = asyncHandler (async (req,res) => {
+
+    const clients = await Client.find({});
+    if(clients.length === 0){
+        return res.status(400).json({
+            message:"No clients in the DB !"
+        })
+    }
+
+    res.status(200).json({
+        clients
+    })
 })
--- Project_Codes/PFE_Project/Backend_Project_Codes/routes/clientRoute.js
+++ Project_Codes/PFE_Project/Backend_Project_Codes/routes/clientRoute.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const {createClientCtrl} = require('../controllers/clientController.js')
+const {createClientCtrl, getAllClientCtrl} = require('../controllers/clientController.js')
 
+//Create client
 router.route("/create").post(createClientCtrl)
+
+// get all clients
+router.route("/getAll").get(getAllClientCtrl)
 
 module.exports = router;
