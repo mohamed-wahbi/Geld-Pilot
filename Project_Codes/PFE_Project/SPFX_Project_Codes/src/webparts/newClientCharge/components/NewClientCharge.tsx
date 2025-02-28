316767937b0ba6ecee9f4d9dd10b0f353184d088
mohamed-wahbi <wahbisj@gmail.com>
2025-02-28 13:12:50 +0100

    Stabilization of the uniform dashboard template for the Client and Monthly Charges Management System web part.

--- Project_Codes/PFE_Project/SPFX_Project_Codes/src/webparts/newClientCharge/components/NewClientCharge.tsx
+++ Project_Codes/PFE_Project/SPFX_Project_Codes/src/webparts/newClientCharge/components/NewClientCharge.tsx
@@ -1,16 +1,37 @@
 import * as React from 'react';
-import styles from './NewClientCharge.module.scss';
+import { useEffect, useState } from 'react';
+import styles from "./NewClientCharge.module.scss";
 const AOS = require("aos");
 import "aos/dist/aos.css";
 
 const NewClientCharge: React.FC = () => {
-  React.useEffect(() => {
+  const [activeTab, setActiveTab] = useState('partners');
+
+  useEffect(() => {
     AOS.init({ duration: 1500, once: true });
   }, []);
 
   return (
-    <div className={styles.NewClientChargeComp}>
-        NewClientChargeComp
+    <div className={styles.DashComp}>
+      <div className={styles.headerDash}>
+        <div className={styles.titleDash}>
+          <h5 className={styles.titleTextDash}>Client And Monthly Charges Management Systemt</h5>
+          <img src={require('../assets/logo-removebg-preview.png')} alt='logo' className={styles.logoImgDash} />
+        </div>
+        <div className={styles.navLinks}>
+          <p className={`${styles.link} ${activeTab === 'partners' ? styles.active:null}`} onClick={() => setActiveTab('partners')}>New Client</p>
+          <p className={`${styles.link} ${activeTab === 'create' ? styles.active:null}`} onClick={() => setActiveTab('create')}>New Charge</p>
+        </div>
+      </div>
+      <div >
+        {activeTab === 'partners' && (
+          <div>1</div>
+        )}
+
+        {activeTab === 'create' && (
+          <div>2</div>
+        )}
+      </div>
     </div>
   );
 };
