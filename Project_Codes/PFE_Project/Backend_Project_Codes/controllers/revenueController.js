4ec357598be259dd8590e4f12f577096c21ca1b5
mohamed-wahbi <wahbisj@gmail.com>
2025-03-05 09:41:50 +0100

    Creation of the Generated Revenue function for Generate Revenues with Year and Month Params.

--- Project_Codes/PFE_Project/Backend_Project_Codes/controllers/revenueController.js
+++ Project_Codes/PFE_Project/Backend_Project_Codes/controllers/revenueController.js
@@ -1,5 +1,6 @@
 const asyncHandler = require("express-async-handler");
-const {Revenue} =  require ("../models/RevenueModel")
+const {Revenue} =  require ("../models/RevenueModel");
+const { Invoice } = require("../models/invoiceModel");
 
 /*--------------------------------------------------
 * @desc    Get All revenues
@@ -17,4 +18,61 @@ module.exports.getRevenuesCtrl = asyncHandler(async(req,res)=> {
     res.status(200).json({
         message : "revenues featched !"
     })
+})
+
+
+
+/*--------------------------------------------------
+* @desc    generate Monthly Revenue
+* @router  /api/revenue/generate
+* @methode POST
+* @access  only admin
+----------------------------------------------------*/
+module.exports.generateRevCtrl = asyncHandler(async(req,res) => {
+
+    const {annee, mois} = req.params
+
+    try {
+        const startDate = new Date(`${annee}-${mois}-01T00:00:00.000Z`);
+        const endDate = new Date(startDate);
+        endDate.setMonth(endDate.getMonth() + 1);
+
+        const invoices = await Invoice.find({
+            statut: 'discharged',
+            datePaiementEntreprise: { $gte: startDate, $lt: endDate }
+        });
+
+        const groupedData = invoices.reduce((acc, invoice) => {
+            const key = `${annee}-${mois}-${invoice.id_client}`;
+
+            if (!acc[key]) {
+                acc[key] = {
+                    annee:`${annee}`,
+                    mois: `${mois}`,
+                    id_client: invoice.id_client,
+                    nomClient: invoice.clientName,
+                    nombreFacturesPayees: 0,
+                    montantTotalPaye: 0,
+                };
+            }
+
+            acc[key].nombreFacturesPayees += 1;
+            acc[key].montantTotalPaye += invoice.montantPaye;
+
+            return acc;
+        }, {});
+
+        const revenues = Object.values(groupedData);
+        await Revenue.insertMany(revenues);
+        res.status(200).json({
+            message: "Revenue Generated successfuly .",
+            annee,
+            mois
+        })
+    } catch (err) {
+        console.error("Erreur lors de la génération des revenus mensuels", err);
+    }
+
+   
+
 })
