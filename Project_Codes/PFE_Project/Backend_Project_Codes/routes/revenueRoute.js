b49b814ad84ec2a70a9e22efb0bf594b4f6936a7
mohamed-wahbi <wahbisj@gmail.com>
2025-03-05 09:44:24 +0100

    creation of the Generate Revenue Route Path with 2 Params annee et mois .

--- Project_Codes/PFE_Project/Backend_Project_Codes/routes/revenueRoute.js
+++ Project_Codes/PFE_Project/Backend_Project_Codes/routes/revenueRoute.js
@@ -1,8 +1,11 @@
 const express = require('express');
-const { getRevenuesCtrl } = require('../controllers/revenueController');
+const { getRevenuesCtrl, generateRevCtrl } = require('../controllers/revenueController');
 const router = express.Router();
 
 router.route("/getAll").get(getRevenuesCtrl)
+
+router.route("/generate/:annee/:mois").post(generateRevCtrl)
+
 
 
 module.exports = router;
