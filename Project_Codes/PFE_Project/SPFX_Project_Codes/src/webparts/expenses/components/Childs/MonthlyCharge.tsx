65d9d39fbc01d0d8c0fdf7bb6a4350b72efaea75
mohamed-wahbi <wahbisj@gmail.com>
2025-03-09 13:40:13 +0100

    Recovery of all created fixed charges, and configuration of a section to collect information from the new monthly charge file to be filled.

--- Project_Codes/PFE_Project/SPFX_Project_Codes/src/webparts/expenses/components/Childs/MonthlyCharge.tsx
+++ Project_Codes/PFE_Project/SPFX_Project_Codes/src/webparts/expenses/components/Childs/MonthlyCharge.tsx
@@ -1,92 +1,167 @@
 import * as React from 'react';
-import { useState } from 'react';
- const AOS = require("aos");
-import "aos/dist/aos.css";
+import { useState, useEffect } from 'react';
+import 'aos/dist/aos.css';
 import styles from '../Expenses.module.scss';
-const { VocaFlexMWSTn } = require('vecoflextnmws')
-
-
-
+import axios from 'axios';
+const { VocaFlexMWSTn } = require('vecoflextnmws');
 
 interface Expense {
-  
+  expenseName: string;
+  expenseType: string;
 }
 
-
-
-
-
+interface Charge {
+  _id: string;
+  expenseName: string;
+}
 
 const MonthlyCharge: React.FC = () => {
+  const [year, setYear] = useState('');
+  const [month, setMonth] = useState('');
+  const [selectedCharges, setSelectedCharges] = useState<string[]>([]); // Charges sélectionnées
+  const [charges, setCharges] = useState<Charge[]>([]); // Liste des charges
+  const [generatChargeFixTab, setGeneratChargeFixTab] = useState(false);
 
-    // -----------------------Filter System--------------------------------
-    const [allFiltredDatas, setAllFiltredDatas] = useState<Expense[]>([])
-    const [oneFiltredData, setOneFiltredData] = useState<Expense | null>(null)
-  
-    console.log(allFiltredDatas);// liste des données filtre
-  
-    console.log(oneFiltredData)// un ligne de la liste des données filtré
-  
-  
-    const getAllFiltredDatas = (data: Expense[]) => { setAllFiltredDatas(data) }
-    const getOneFiltredData = (data: Expense) => { setOneFiltredData(data) }
-    // ___________________________________________________________________
- 
+  console.log(charges);
 
-  React.useEffect(() => {
-    AOS.init({ duration: 1500, once: true });
+  useEffect(() => {
+    fetchCharges();
   }, []);
+
+  // Fonction pour récupérer toutes les charges fixes
+  const fetchCharges = async () => {
+    try {
+      const res = await axios.get('http://127.0.0.1:3320/api/expense-fix/getAll');
+      setCharges(res.data.Expenses_Fixs);
+    } catch (error) {
+      console.error('Erreur lors de la récupération des charges fixes !', error);
+    }
+  };
+
+  // ----------------------- Filter System --------------------------------
+  const [allFiltredDatas, setAllFiltredDatas] = useState<Expense[]>([]);
+  const [oneFiltredData, setOneFiltredData] = useState<Expense | null>(null);
+
+  console.log(allFiltredDatas); // liste des données filtrées
+  console.log(oneFiltredData); // une ligne de la liste des données filtrées
+
+  const getAllFiltredDatas = (data: Expense[]) => {
+    setAllFiltredDatas(data);
+  };
+  const getOneFiltredData = (data: Expense) => {
+    setOneFiltredData(data);
+  };
+
+  // ------------------- Handel Select Charges -----------------------------
+  const handleChargeSelection = (chargeId: string) => {
+    setSelectedCharges((prevSelected) =>
+      prevSelected.includes(chargeId)
+        ? prevSelected.filter((id) => id !== chargeId) // Désélectionner
+        : [...prevSelected, chargeId] // Sélectionner
+    );
+  };
+
+
 
   return (
     <div className={styles.tableContainer}>
-    
-
       <div className={styles.HeaderTabelCtrl}>
         <div className={styles.searchInput}>
           <VocaFlexMWSTn
-            data={""}
-            keys={[""]}
-            lang={"en-US"}
-            threshold={"0.4"}
+            data={''}
+            keys={['']}
+            lang={'en-US'}
+            threshold={'0.4'}
             allFiltredDatas={getAllFiltredDatas}
             oneFiltredData={getOneFiltredData}
-            titre={"expenseName"}
-            description={"expenseType"}
+            titre={'expenseName'}
+            description={'expenseType'}
           />
-
         </div>
 
-       
+        <div className={styles.generateRevenue}>
+          <div className={styles.Top}>
+            <p>Generat Monthly Expenses</p>
+            <button
+            style={{borderRadius: "5px"}}
+              onClick={() => {
+                setGeneratChargeFixTab(!generatChargeFixTab);
+                setYear('');
+                setMonth('');
+              }}
+            >
+              {generatChargeFixTab ? '❌' : '🆕'}
+            </button>
+          </div>
+
+          {generatChargeFixTab && (
+            <div className={styles.GenerateForm}>
+              <div className={styles.inputGenerateForm}>
+                <label>Year</label>
+                <input
+                  style={{ marginLeft: "15px" }}
+                  placeholder="2025"
+                  value={year}
+                  onChange={(e) => setYear(e.target.value)}
+                  required
+                />
+              </div>
+
+              <div className={styles.inputGenerateForm}>
+                <label>Month</label>
+                <input
+                  placeholder="01 - 12"
+                  value={month}
+                  onChange={(e) => setMonth(e.target.value)}
+                  required
+                />
+              </div>
+
+              <div >
+                <ul className={styles.inputGenerateFormUl}>
+                  {charges.map((charge) => (
+                    <li key={charge._id}>
+
+                      <input
+                        type="checkbox"
+                        onChange={() => handleChargeSelection(charge._id)}
+                        checked={selectedCharges.includes(charge._id)}
+                      />
+                      {charge.expenseName}
+
+                    </li>
+                  ))}
+                </ul>
+              </div>
+
+              <div className={styles.btnContent}>
+                <button style={{ border: 'none', margin: " 0px 10px", borderRadius: "5px" }}>➕</button>
+              </div>
+            </div>
+          )}
+        </div>
       </div>
 
       <div className={styles.TableContent}>
         <table className={styles.table}>
           <thead>
             <tr>
-              <th>Controls</th>
-              <th>Expense Name</th>
-              <th>Expense Type</th>
-              <th>Amount</th>
-              <th>Status</th>
-              <th>Payment Day</th>
-              <th>Created Date</th>
+              <th>Année</th>
+              <th>Mois</th>
+              <th>Nom Charge</th>
+              <th>Type Charge</th>
+              <th>Montant Prévisionnel (€)</th>
+              <th>Montant Réel (€)</th>
+              <th>Cavred Day</th>
+              <th>Statut Charge</th>
+              <th>Contrôle</th>
             </tr>
           </thead>
           <tbody>
-
             {/* Ligne pour ajouter un nouveau client en haut du tableau */}
-            
-
-
-
-
-
-            
           </tbody>
         </table>
       </div>
-
-
     </div>
   );
 };
