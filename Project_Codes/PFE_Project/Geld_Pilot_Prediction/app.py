e0aa5bb67e20f9ba1ccbd85d910ebb137869bda0
mohamed-wahbi <wahbisj@gmail.com>
2025-03-24 23:48:26 +0100

    Connection to Geld_Pilot mongodb Data Base And get all Datas from monthlyfinancetrainmls and annualfinancetrainmls models using pymongo, and storage them in 2 data freams df_monthly and df_annual with pandas PD Lib.

--- Project_Codes/PFE_Project/Geld_Pilot_Prediction/app.py
+++ Project_Codes/PFE_Project/Geld_Pilot_Prediction/app.py
@@ -8,6 +8,31 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+# Connexion MongoDB
+client = MongoClient("mongodb://127.0.0.1:27017/")
+db = client["Geld_Pilot"]
+
+# Charger les données
+def get_finance_data():
+    monthly_data = list(db.monthlyfinancetrainmls.find({}, {"_id": 0}))
+    annual_data = list(db.annualfinancetrainmls.find({}, {"_id": 0}))
+
+    df_monthly = pd.DataFrame(monthly_data)
+    df_annual = pd.DataFrame(annual_data)
+
+    if df_monthly.empty:
+        print("⚠️ Aucune donnée récupérée depuis MonthlyFinanceTrainML !")
+    else:
+        print("Colonnes disponibles dans df_monthly :", df_monthly.columns)
+
+    if df_annual.empty:
+        print("⚠️ Aucune donnée récupérée depuis AnnualFinanceTrainML !")
+    else:
+        print("Colonnes disponibles dans df_annual :", df_annual.columns)
+
+    return df_monthly, df_annual
+
+
 
 
 if __name__ == "__main__":
